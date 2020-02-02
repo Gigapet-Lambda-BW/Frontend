de094fac7554c06af58cba8c9c0918d2b04e67a9
Simone Roy <simonecarolynr@gmail.com>
2020-02-02 00:39:47 -0700

    user-settings-started

---
+++ gigapet/src/UserSettingsForm.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Form, Formik, Field } from 'formik';
+import Button from './Button';
+
+export default function UserSettingsForm() {
+    return (
+      <Formik>
+            <Form className='settings-form'>
+
+                <Field component='select' name='child'>
+                  <option>Choose a child</option>
+                  <option>Child 1</option>
+                  <option>Child 2</option>
+                </Field>
+
+                <h3>Allergies</h3>
+                <label>Check all that apply:</label>
+
+                <label>Nuts
+                    <Field type='checkbox'></Field>
+                </label>
+                <label>Eggs
+                    <Field type='checkbox'></Field>
+                </label>
+                <label>Dairy
+                    <Field type='checkbox'></Field>
+                </label>
+
+                <Button />
+            </Form>
+      </Formik>
+    )
+}
